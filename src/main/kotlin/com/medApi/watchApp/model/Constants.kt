2991699fbc7dde63955904f68f4d0f4e4f534dc7
Пожилой Жмых <vovanberman@gmail.com>
2023-11-07 19:54:53 +0300

    Names changed

--- src/main/kotlin/com/medApi/watchApp/model/Constants.kt
+++ src/main/kotlin/com/medApi/watchApp/model/Constants.kt
@@ -3,6 +3,6 @@ package com.medApi.watchApp.model
 import java.util.ArrayList
 
 object Constants {
-    val personList: ArrayList<String> = arrayListOf("Aub", "Dima", "Salah", "Tamara", "Tigran")
+    val personList: ArrayList<String> = arrayListOf("Aub", "Dmitry", "Salah", "TamaraSalamonovna", "Tigran")
     val imagePath: String = "/home/aliud/IdeaProjects/watchApp/src/main/resources/static/Labeled"
 }
